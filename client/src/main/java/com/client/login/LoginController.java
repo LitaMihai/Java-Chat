83c470a8c61f5d13af42e850781b456cbcc6ebce
Mihai <litamihai13@gmail.com>
2023-03-19 12:56:12 +0200

    Implemented login page design: Added CSS file for the login page design. Ready for integration with backend authentication functionality.

--- client/src/main/java/com/client/login/LoginController.java
+++ client/src/main/java/com/client/login/LoginController.java
@@ -1,14 +1,61 @@
 package com.client.login;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+    @FXML
+    private BorderPane borderPane;
+    private double xOffset;
+    private double yOffset;
+
+    private static LoginController instance;
+
+    public LoginController() {
+        instance = this;
+    }
+
+    public static LoginController getInstance() {
+        return instance;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        /* Drag and Drop */
+        borderPane.setOnMousePressed(event -> {
+            xOffset = MainLauncher.getPrimaryStage().getX() - event.getScreenX();
+            yOffset = MainLauncher.getPrimaryStage().getY() - event.getScreenY();
+            borderPane.setCursor(Cursor.CLOSED_HAND);
+        });
 
+        borderPane.setOnMouseDragged(event -> {
+            MainLauncher.getPrimaryStage().setX(event.getScreenX() + xOffset);
+            MainLauncher.getPrimaryStage().setY(event.getScreenY() + yOffset);
+
+        });
+
+        borderPane.setOnMouseReleased(event -> {
+            borderPane.setCursor(Cursor.DEFAULT);
+        });
+    }
+
+    public void loginButtonAction() throws IOException {
+        System.out.println("LOGGED IN");
+    }
+
+    public void closeSystem(){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void minimizeWindow(){
+        MainLauncher.getPrimaryStage().setIconified(true);
     }
 }
