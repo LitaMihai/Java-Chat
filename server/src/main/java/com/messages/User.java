22692ccf4c962d860600d32e8c2cb8f3780683e1
Mihai <litamihai13@gmail.com>
2023-03-27 20:29:49 +0300

    Created the server side. Started implementing chat design.

---
+++ server/src/main/java/com/messages/User.java
@@ -0,0 +1,33 @@
+package com.messages;
+
+public class User {
+    private String name;
+    private String picture;
+    private Status status;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        if(name.isEmpty() == false)
+            this.name = name;
+    }
+
+    public String getPicture(){
+        return this.picture;
+    }
+
+    public void setPicture(String picture){
+        if(picture.isEmpty() == false)
+            this.picture = picture;
+    }
+
+    public Status getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(Status status){
+        this.status = status;
+    }
+}
--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.server {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.slf4j;
 
 
     opens com.example.server to javafx.fxml;
